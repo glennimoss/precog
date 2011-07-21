c73dd93b1745ec171fcd2926931084bee30eb056
Glenn Moss <gim@keynetics.com>
2011-07-21 17:07:23 -0600

    Initial sketch of code

---
+++ myschema.py
@@ -0,0 +1,22 @@
+from OracleObjects import *
+
+foo = Table('foo',
+      [ ('id', 'number')
+      , ('text', 'varchar2(100)') ])
+
+bar = Table('bar', 
+      [ ('id', 'number') 
+      , ('foo_id', 'number')
+      , ('body', 'varchar2(32)') ])
+
+schema = [foo, bar]
+
+for t in schema:
+  try:
+    print(t)
+    print('Created:', bool(t.create()))
+    print()
+  except TypeConflict as e:
+    print(e)
+
+
