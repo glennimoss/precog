b587017d843cb039a9fcf1ff624afd39b32ad4fc
Glenn Moss <glennimoss+github@gmail.com>
2011-08-10 02:17:49 -0600

    Trying out some mixin ideas to prove they work

--- testclass.py
+++ testclass.py
@@ -1,17 +1,46 @@
-def foo (arg):
-  def _foo (self):
-    print(arg)
-  return _foo
+def test_property_mixin ():
+  class A (object):
+    @property
+    def foo (self):
+      print('A.foo getter')
+      return self._foo
 
-class A (object):
-  def __init__ (self):
-    self.foo = foo('A')
+    @foo.setter
+    def foo (self, foo):
+      print('A.foo setter')
+      self._foo = 'A' + foo
 
-class B (object):
-  def __init__ (self):
-    self.foo = foo('B')
+  class B (A):
 
-a = A()
-b = B()
-a.foo()
-b.foo()
+    @A.foo.setter
+    def foo (self, foo):
+      print('B.foo setter')
+      A.foo.__set__(self, 'B' + foo)
+
+  a = A()
+  a.foo = 'cool'
+  print("a.foo = {}".format(a.foo))
+
+  b = B()
+  b.foo = 'beans'
+  print("b.foo = {}".format(b.foo))
+
+def test_mixin_mro ():
+  class A (object):
+    def foo (self):
+      print('A.foo')
+
+  class Mixin (object):
+    def foo (self):
+      print('Mixin.foo')
+      super().foo()
+
+  class B (Mixin, A):
+    def foo (self):
+      print('B.foo')
+      super().foo()
+
+  b = B()
+  b.foo()
+
+test_mixin_mro()
