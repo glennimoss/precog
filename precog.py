cc4ade2498b2319030314a7983aac59814fa959d
Glenn Moss <gim@keynetics.com>
2011-08-11 16:05:27 -0600

    Make the toplevel executable act like a real command-line tool.

--- precog.py
+++ precog.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-
-import argparse, logging, os, sys
+import argparse, logging, os, re, sys
 
 from precog.objects import Database
-from precog.errors import OracleError
+from precog.errors import PrecogError, OracleError
 
-# TODO: silly os doesn't pass in COLUMNS envvar... so 80 is assumed :(
+#Always print help
 class HelpyArgparser(argparse.ArgumentParser):
   def error (self, message):
     if len(sys.argv) != 1:
@@ -13,17 +11,48 @@ class HelpyArgparser(argparse.ArgumentParser):
     self.print_help()
     sys.exit(1)
 
-parser = HelpyArgparser(description='Generate Oracle migration script.',
-    formatter_class=argparse.RawTextHelpFormatter)
+# Validate connection string
+class ConnectionStringAction (argparse.Action):
+  metavar = '<username>/<password>@<dsn>'
+  def __call__ (self, parser, namespace, values, option_string=None):
+    match = re.match(r'^("[^"]+"|[\w$#]+)/("[^"]+"|[^/@]+)@(.+)$', values)
+    if not match:
+      parser.error("Connection string does not match {}".format(self.metavar))
+
+    setattr(namespace, self.dest, values)
+    namespace.username, namespace.password, namespace.dsn = match.groups()
+
+
+parser = HelpyArgparser(description='Generate Oracle migration script.')
+
+
+# Positional arguments
 parser.add_argument('connect_string',
-    metavar='<username>/<password>@<connect_identifier>', type=str,
+    metavar=ConnectionStringAction.metavar, action=ConnectionStringAction,
     help='Oracle connection string')
 parser.add_argument('file', type=argparse.FileType('r'),
-    help='SQL*Plus script to parse')
-parser.add_argument('-v', '--verbose', action='append_const', const=True,
+    help='SQL*Plus script to parse'
+    )
+
+# Output control options
+output_group = parser.add_mutually_exclusive_group()
+output_group.add_argument('-v', '--verbose', action='append_const', const=True,
     help='Verbose logging. Specify twice for more output.')
-parser.add_argument('-q', '--quiet', action='store_true',
+output_group.add_argument('-q', '--quiet', action='store_true',
     help='Suppress output')
+
+# Configuration options
+parser.add_argument('--schema',
+    help='Schema name for unqualified object names. Defaults to <username>.')
+
+# User-input options
+prompt_group = parser.add_mutually_exclusive_group()
+prompt_group.add_argument('-y', '--apply', action='store_true',
+  help='Apply all changes without asking.')
+prompt_group.add_argument('-n', '--no-apply', action='store_true',
+  help='Do not ask or apply any changes.')
+
+# Now GO!
 args = parser.parse_args()
 
 log_config = {'style': '{', 'format': '{levelname}: {message}'}
@@ -41,7 +70,9 @@ else:
 logging.basicConfig(**log_config)
 
 try:
-  schema_name = args.connect_string.split('/')[0]
+  schema_name = args.username
+  if args.schema:
+    schema_name = args.schema
   database = Database.from_file(args.file, schema_name)
 
   diffs = database.diff_to_db(args.connect_string)
@@ -50,7 +81,10 @@ try:
     print("Found {} changes:".format(len(diffs)))
     print(";\n\n".join(str(diff) for diff in diffs) + ";\n")
 
-    doit = input('Run script? [yN] ')
+    if not (args.apply or args.no_apply):
+      doit = input('Run script? [yN] ')
+    else:
+      doit = 'y' if args.apply else 'n'
 
     errors = 0
     if 'y' == doit.lower():
@@ -66,5 +100,5 @@ try:
 
   else:
     print("Oracle is up to date with {}".format(args.file.name))
-except Exception as e:
+except PrecogError as e:
   print(e)
