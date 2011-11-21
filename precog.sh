5506c4da2f415d1d5697c65e8d2c357dd8f153ed
Glenn Moss <gim@keynetics.com>
2011-11-21 10:44:20 -0700

    Specificly require python 3.2

--- precog.sh
+++ precog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # silly os doesn't pass in COLUMNS envvar...
 # so unless we're explicit, 80 is assumed :(
-COLUMNS=$(tput cols) python3 precog.py "$@"
+COLUMNS=$(tput cols) python3.2 precog.py "$@"
