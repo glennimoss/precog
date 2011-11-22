ab369ec3d6a541232c7c3f640775eaa628a48772
Glenn Moss <gim@keynetics.com>
2011-11-22 15:38:09 -0700

    Run precog from anywhere! Previously, you had to actually chdir into the precog root.

--- precog.sh
+++ precog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # silly os doesn't pass in COLUMNS envvar...
 # so unless we're explicit, 80 is assumed :(
-COLUMNS=$(tput cols) python3.2 precog.py "$@"
+BASE=$(dirname "$(realpath "$0")")
+COLUMNS=$(tput cols) PYTHONPATH="$PYTHONPATH:$BASE" python3.2 $BASE/precog.py "$@"
