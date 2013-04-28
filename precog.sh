0df3e10ab37e878c34920838e6e27c310e09c1aa
Glenn Moss <glennimoss+github@gmail.com>
2013-04-28 15:06:38 -0600

    Clean up root directory, and eliminate the precog.sh/precog.py duality.

--- precog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# silly os doesn't pass in COLUMNS envvar...
-# so unless we're explicit, 80 is assumed :(
-BASE=$(dirname "$(realpath "$0")")
-COLUMNS=$(tput cols) PYTHONPATH="$PYTHONPATH:$BASE" python3.2 $BASE/precog.py "$@"
